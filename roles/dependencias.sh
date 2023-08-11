f82c070b7a652e3d43a5854a2c091f0bff3f688e
@ivan.uribe <ivan.uribe@solem.cl>
2023-08-11 08:39:14 -0500

    dependencias

--- roles/dependencias.sh
+++ roles/dependencias.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
+# Verificación de Dependencias
+# Autor: Ivan Uribe Gonzalez
+# Usage: ./dependencias.sh
 
 # Verificación de eksctl
 if ! command -v eksctl &> /dev/null; then
     echo "eksctl no está instalado. Por favor, sigue las instrucciones para instalarlo."
     exit 1
+else
+    eksctl_version=$(eksctl version 2>&1)
+    echo "eksctl versión: $eksctl_version"
 fi
 
 # Verificación de kubectl
 if ! command -v kubectl &> /dev/null; then
     echo "kubectl no está instalado. Por favor, sigue las instrucciones para instalarlo."
     exit 1
+else
+    kubectl_version=$(kubectl version --client --output=yaml | grep -o "kustomizeVersion:.*" 2>&1)
+    echo "kubectl versión: $kubectl_version"
 fi
 
 # Verificación de ansible
 if ! command -v ansible &> /dev/null; then
     echo "ansible no está instalado. Por favor, sigue las instrucciones para instalarlo."
     exit 1
-fi
-
-# Verificación de awscli
-if ! command -v awscli &> /dev/null; then
-    echo "eksctl no está instalado. Por favor, sigue las instrucciones para instalarlo."
-    exit 1
+else
+    ansible_version=$(ansible --version | grep "ansible [core" | awk '{print $3}')
+    echo "Versión de Ansible: $ansible_version"
 fi
 
 # Verificación de otras herramientas y dependencias aquí...
-echo "Todas las dependencias están instaladas correctamente. Puedes ejecutar el playbook."
+echo "Todas las dependencias están instaladas correctamente. Puedes ejecutar el playbook"
